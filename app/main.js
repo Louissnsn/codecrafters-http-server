4899289506fe44da8b78421f58a25c79d9b273b3
Louissnsn <sanson.louis@gmail.com>
2025-04-02 11:58:00 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,15 +1,14 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+
+
   socket.on("data", (data) => {
-    console.log("data", data);
+    const lines = data.toString().split("\r\n");
+     console.log("lines", lines);
     const path = data.toString().split(" ")[1];
     if (path == "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
@@ -17,11 +16,24 @@ const server = net.createServer((socket) => {
       const content = path.split("/echo/")[1];
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
-      );
-    } else {
+      )
+    } 
+    else if(path.includes("user-agent")){
+      console.log("User-Agent has been found in the path");
+      const userAgent = lines[2].split(": ")[1];
+      console.log("User-Agent", userAgent);
+
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+      )
+    }
+     else {
       socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
     }
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, () => {
+  console.log("Server is running on port 4221");
+}
+);
